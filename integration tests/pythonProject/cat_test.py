63e6a884e82646e22c08c49de5cb140e9907f176
izykov <izykov@ds-in.net>
2024-12-07 12:12:21 +0100

    int tests

--- integration tests/pythonProject/cat_test.py
+++ integration tests/pythonProject/cat_test.py
@@ -22,6 +22,71 @@ def test_big_text():
 def test_case_b():
     file_path = "cat/tests/case_b"
     main_test(file_path)
-    
+
 def test_case_e():
     file_path = "cat/tests/case_e"
+    main_test(file_path)
+
+def test_case_n():
+    file_path = "cat/tests/case_n"
+    main_test(file_path)
+
+def test_case_s():
+    file_path = "cat/tests/case_s"
+    main_test(file_path)
+
+def test_case_t():
+    file_path = "cat/tests/case_t"
+    main_test(file_path)
+
+def test_case_upE():
+    file_path = "cat/tests/case_upE"
+    main_test(file_path)
+
+def test_case_upT():
+    file_path = "cat/tests/case_upT"
+    main_test(file_path)
+
+def test_hard_case():
+    file_path = "cat/tests/hard_case"
+    main_test(file_path)
+
+def test_memory_case():
+    file_path = "cat/tests/memory_case"
+    main_test(file_path)
+
+def test_case_new():
+    file_path = "cat/tests/new"
+    main_test(file_path)
+
+def test_case_nonprint():
+    file_path = "cat/tests/nonprint"
+    main_test(file_path)
+
+def test_orig_case_b():
+    file_path = "cat/tests/orig_case_b"
+    main_test(file_path)
+
+def test_orig_case_e():
+    file_path = "cat/tests/orig_case_e"
+    main_test(file_path)
+
+def test_orig_case_n():
+    file_path = "cat/tests/orig_case_n"
+    main_test(file_path)
+
+def test_orig_case_s():
+    file_path = "cat/tests/orig_case_s"
+    main_test(file_path)
+
+def test_orig_case_t():
+    file_path = "cat/tests/orig_case_t"
+    main_test(file_path)
+
+def test_case_out3():
+    file_path = "cat/tests/out3"
+    main_test(file_path)
+
+def test_out4():
+    file_path = "cat/tests/out4"
+    main_test(file_path)
